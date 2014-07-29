1ef0849cf5dd85908b24262fde537742da3d177f
Ke Xu <kxu1985@gmail.com>
2014-07-29 11:18:21 -0400

    update event driven

--- Self_Ch1_EventDriven/EventMachine.java
+++ Self_Ch1_EventDriven/EventMachine.java
@@ -17,5 +17,34 @@ public class EventMachine
 			this.type = type;
 			this.data = data;
 		}
+
 	}
+    public static void printEventA(Event e)
+    {
+        System.out.println(e.data);
+    }
+
+    public static void printEventB(Event e)
+    {
+        System.out.println(e.data.toUpperCase());
+    }
+
+    public static void main(String[] args)
+    {
+        Queue<Event> events = new LinkedList<Event>();
+        events.add(new Event('A',"Hello"));
+        events.add(new Event('B',"event-driven"));
+        events.add(new Event('A',"World!"));
+
+        Event e;
+        while (!events.isEmpty())
+        {
+            e = events.remove();
+
+            if (e.type == 'A')
+                printEventA(e);
+            if (e.type == 'B')
+                printEventB(e);
+            }
+        }
 }
