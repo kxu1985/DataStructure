3d8b9957b179ee3f2ab6aa9f59ab206bc09a14cc
Ke Xu <kxu1985@gmail.com>
2014-08-01 08:44:25 -0400

    add ch2 q5

---
+++ Ch2_Questions/Q5.java
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------------------
+* Q5: You have two numbers represented by a linked list, where each node contains a single
+*     digit. The digits are stored in reverse order, such that the 1's digit is at the 
+*     head of the list. Write a function that adds the two numbers and returns the sum as 
+*     a linked list.
+* Example:
+*     Input: (7->1->6) + (5->9->2). That is, 617 + 295.
+*     Output: 2->1->9. That is, 912.
+* Follow up
+*     Suppose the digits are stored in forward order. Repeat the above problem.
+* Example:
+*     Input: (6->1->7) + (2->9->5). That is, 617 + 295.
+*     Output: 9->1->2. That is, 912.
+*----------------------------------------------------------------------------------------*/
+
+public class Q5
+{
+	public static SLNode addLists(SLNode l1, SLNode l2)
+	{
+		SLNode result = null;
+
+		while (!(l1 == null && l2 == null))
+		{
+			int carry = 0;
+			int l1_value = 0;		
+		}
+	}
+
+
+	public static void main(String[] args)
+	{
+
+	}
+}
